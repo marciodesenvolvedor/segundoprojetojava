757d818e6d583766f9bd6bd299ed73017389de12
marciodesenvolvedor <89565704+marciodesenvolvedor@users.noreply.github.com>
2021-08-30 15:50:11 -0300

    Add files via upload

---
+++ Classe Scanner Java.java
@@ -0,0 +1,15 @@
+﻿import java.util.*;
+
+public class Programa
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String texto = scan.nextLine();
+        int inteiro = scan.nextInt();
+
+        System.out.println(texto);
+        System.out.println(inteiro);
+        
+        scan.close();
+    }
+}
